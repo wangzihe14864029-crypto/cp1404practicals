df261cdf9be878ec445585a12b962518dce1cdd6
WANG ZIHE <wangzihe14864029@gmail.com>
2025-11-02 21:08:51 +0800

    Add guitar program

---
+++ prac_06/guitar.py
@@ -0,0 +1,30 @@
+"""
+CP1404 Practical
+Estimate: 10 minutes
+Actual: 10 minutes
+"""
+
+CURRENT_YEAR = 2025
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the guitar's age in years."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old."""
+        return self.get_age() >= VINTAGE_AGE
